7127a71c7a96a2a3fe38e21c2f6882fd51d35607
pop_om <moreno.x.omar@proton.me>
2026-02-28 13:14:49 -0800

    finished basic setup of playarea and entities

---
+++ main.lua
@@ -0,0 +1,54 @@
+-- purpose: create simple play area
+-- to do later:
+--      move resolution to conf.lua
+
+-- use percentage of resolution for position of others
+-- allowing variables to be accessed by functions in file
+-- not global. won't get problems with other files with same variable names
+
+local width = 360
+local length = 480
+local offset = 20
+local boss_pos_x = (width + offset) / 2
+boss_pos_y = (length + offset) / 8
+local y = 0
+
+function love.draw()
+    -- include offset, since 0,0 is top left of screen
+    -- dimensions are 360x480
+    love.graphics.rectangle("line", 20, 20, 360, 480)
+
+    --[[ use percentage of resolution for position of others
+    width = 360
+    length = 480
+    offset = 20
+    boss__pos_x = (width + offset) / 2
+    boss_pos_y = (length + offset) / 2
+    --]]
+
+    -- position first 2 after mode, size after those
+    -- consider this boss, move to update later
+    love.graphics.rectangle("fill", boss_pos_x, boss_pos_y, 5, 5)
+
+    --[[ bullet going straight down
+    love.graphics.circle("fill", boss_pos_x, (boss_pos_y + y), 5, 5)
+    --]]
+
+    -- radiating pattern?
+    love.graphics.circle("line", boss_pos_x, boss_pos_y, (5 + y), 5)
+end
+
+function love.update(dt)
+    -- necessary for bullets and movement
+    -- for easy use, just use default dt
+    -- goal: 60 fps
+
+    -- see current dt
+    -- print(dt)
+
+    -- move down only, which is adding to total
+    -- needs to be multiplied by rate since dt is small
+    y = y + (dt * 100)
+
+    -- love.graphics.circle("fill", boss_pos_x, (boss_pos_y - 5), 50, 5)
+end
